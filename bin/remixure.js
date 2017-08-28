ef4c403343866ba9ee13a340f5fc99cc580cc1bf
gogocrow <gogocrow@163.com>
2017-08-28 17:21:23 +0800

    add url loader for images.

--- bin/remixure.js
+++ bin/remixure.js
@@ -252,6 +252,17 @@ if (config.less) {
     });
   }
 }
+// "url" loader works like "file" loader except that it embeds assets
+// smaller than specified limit in bytes as data URLs to avoid requests.
+// A missing `test` is equivalent to a match.
+rules.push({
+  test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+  loader: require.resolve('url-loader'),
+  options: {
+    limit: 10000,
+    name: 'static/media/[name].[hash:8].[ext]',
+  },
+});
 
 /** Init the plugin. */
 const plugins = [];
