c822f94a480d1061edb21acfcffa39b75ec6cdd8
gogocorw <gogocrow@163.com>
2017-12-21 13:26:46 +0800

    add ignore image and html hash.

--- src/remixure.js
+++ src/remixure.js
@@ -268,6 +268,13 @@ if (config.less) {
     });
   }
 }
+
+let standardName = '[name].[hash:8].[ext]';
+
+if (!!config.ignoreNameHash || isDev) {
+  standardName = '[name].[ext]';
+}
+
 // "url" loader works like "file" loader except that it embeds assets
 // smaller than specified limit in bytes as data URLs to avoid requests.
 // A missing `test` is equivalent to a match.
@@ -276,7 +283,8 @@ rules.push({
   loader: require.resolve('url-loader'),
   options: {
     limit: 10000,
-    name: 'static/media/[name].[hash:8].[ext]',
+    name: `static/media/${standardName}`,
+
   },
 });
 rules.push({
@@ -289,7 +297,8 @@ rules.push({
   },
   options: {
     limit: 100,
-    name: 'static/html/[name].[hash:8].[ext]',
+    name: `static/html/${standardName}`,
+
   },
 });
 
