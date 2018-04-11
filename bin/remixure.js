21e5bdeb19729fe3560cf0cd60160a232411582d
gogocorw <gogocrow@163.com>
2018-04-11 17:49:02 +0800

    adjust the order of the plugins.

--- bin/remixure.js
+++ bin/remixure.js
@@ -153,8 +153,8 @@ var babelConfig = {
       },
       useBuiltIns: true
     }], [require.resolve('babel-preset-react')]],
-    plugins: [require.resolve('babel-plugin-transform-class-properties'), require.resolve('babel-plugin-transform-object-rest-spread'), require.resolve('babel-plugin-syntax-dynamic-import'), require.resolve('babel-plugin-transform-runtime'), require.resolve('babel-plugin-transform-decorators-legacy')],
-    cacheDirectory: true,
+    plugins: [require.resolve('babel-plugin-transform-decorators-legacy'), require.resolve('babel-plugin-transform-class-properties'), require.resolve('babel-plugin-transform-object-rest-spread'), require.resolve('babel-plugin-syntax-dynamic-import'), require.resolve('babel-plugin-transform-runtime')],
+    cacheDirectory: false,
     compact: true
   }
 };
